064b048873f048c3715263275def1275e9af1a93
Gustav Ung <guaschman@gmail.com>
2017-05-04 16:17:41 +0200

    Added initial HTTP request support

--- app/Graphrec/app/src/main/java/com/app/graphrec/graphrec/MainActivity.java
+++ app/Graphrec/app/src/main/java/com/app/graphrec/graphrec/MainActivity.java
@@ -2,6 +2,18 @@ package com.app.graphrec.graphrec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendRequest(View view) {
+        new ImageDownloadTask().execute();
+    }
+
 }
