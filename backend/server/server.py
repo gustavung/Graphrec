3e4e9a50ea579e246ea4b6fca71a59dd16a52159
Mattias Larsson <mala94@telia.com>
2017-05-10 23:30:22 +0200

    Added binary data stream option

--- backend/server/server.py
+++ backend/server/server.py
@@ -8,13 +8,15 @@ from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.path.join(os.path.join(os.getcwd(), '..'), 'images')
 print("Setting the upload folder to:", UPLOAD_FOLDER)
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 IMAGE_NAME = 'tmp_image'
+EXTENSION = '.png'
 
 print("Creating app")
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['IMAGE_NAME'] = IMAGE_NAME
+app.config['EXTENSION'] = EXTENSION
 
 @app.route('/')
 def hello_world():
@@ -32,7 +34,7 @@ def upload_image():
 		if extension not in ALLOWED_EXTENSIONS:
 			response = "File extension %s not allowed" % extension
 			return response
-		full_path = os.path.join(app.config['UPLOAD_FOLDER'], app.config['IMAGE_NAME'] + extension)
+		full_path = os.path.join(app.config['UPLOAD_FOLDER'], app.config['IMAGE_NAME'] + app.config['EXTENSION'])
 		f.save(full_path)
 		response = evaluate(f)
 
@@ -46,6 +48,27 @@ def upload_image():
 def evaluate(image):
 	return random.choice(['Ascending linear function', 'Descending linear function'])
 
+# Test with: curl --data-binary "@FILEPATH" localhost:5000/upload_stream
+@app.route('/upload_stream', methods=['GET', 'POST'])
+def upload_image2():
+	response = 'Dummy response'
+	if request.method == 'POST':
+		full_path = os.path.join(app.config['UPLOAD_FOLDER'], app.config['IMAGE_NAME'] + app.config['EXTENSION'])
+		with open(full_path, 'bw') as f:
+			chunk_size = 4096
+			while True:
+				chunk = request.stream.read(chunk_size)
+				if len(chunk) == 0:
+					break
+				f.write(chunk)
+			response = evaluate(f)
+
+	if os.path.isfile(full_path):
+		os.remove(full_path)
+	else:
+		print("Error: %s file not found" % full_path)
+
+	return response
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
