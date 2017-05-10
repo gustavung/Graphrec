2544235e0386f098f9bcb0e6c985a576555c50fe
Gustav Ung <guaschman@gmail.com>
2017-05-10 23:33:51 +0200

    app: Implemented partial design

--- app/Graphrec/app/src/main/java/com/app/graphrec/graphrec/MainActivity.java
+++ app/Graphrec/app/src/main/java/com/app/graphrec/graphrec/MainActivity.java
@@ -9,7 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -48,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CAMERA_ACTIVITY && resultCode == RESULT_OK) {
             Uri imageUri = Uri.parse(data.getStringExtra("picture"));
             Bitmap imageBitmap = getBitmapFromUri(imageUri);
-            ImageView view = (ImageView) findViewById(R.id.imageView);
-            view.setImageBitmap(imageBitmap);
+            //ImageView view = (ImageView) findViewById(R.id.imageView);
+            //view.setImageBitmap(imageBitmap);
             sendRequest(imageUri);
         }
     }
